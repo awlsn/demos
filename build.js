21b7c57b04b968ec340aead8c62eca35d57ffc19
awlsn <40430213+awlsn@users.noreply.github.com>
2026-06-02 15:07:14 -0400

    Fix typo in file filter for HTML and TXT files

--- build.js
+++ build.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 fs.mkdirSync("public", { recursive: true });
 
 // Copy all html files into public/
-const files = fs.readdirSync(".").filter(f => f.endsWith(".html") || f,endsWidth(".txt"));
+const files = fs.readdirSync(".").filter(f => f.endsWith(".html") || f.endsWith(".txt"));
 files.forEach(f => fs.copyFileSync(f, `public/${f}`));
 
 // Generate index
